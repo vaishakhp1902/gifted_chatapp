90a2195eb9c5f2a9c65d54904de709bb5d238f49
Vaishakh <vaishakhp1998@gmail.com>
2021-11-29 12:46:50 +0530

    connecting to socket.io from react-native client

---
+++ chat-app/App.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import HomeScreen from "./screens/HomeScreen"
+
+export default function App() {
+
+
+  return (
+    <HomeScreen />
+  );
+}
+
--- chat-app/screens/HomeScreen.js
+++ chat-app/screens/HomeScreen.js
@@ -1,8 +1,14 @@
+
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import io from 'socket.io-client'
 
 export default function App() {
+
+  useEffect(function(){
+    io("http://192.168.1.3:3001")
+  },[])
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
