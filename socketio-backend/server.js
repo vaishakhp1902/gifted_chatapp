17f29149f0302b0ad3ae55431328900823e54e81
Vaishakh <vaishakhp1998@gmail.com>
2021-12-01 12:14:33 +0530

    adding unique avatars

--- socketio-backend/server.js
+++ socketio-backend/server.js
@@ -6,6 +6,13 @@ let currentUserId = 2
 
 const users = {}
 
+function createUserAvatarUrl() {
+  const rand1 = Math.round(Math.random() * 200 +100)
+  const rand2 = Math.round(Math.random() * 200 +100)
+ return `https://placeimg.com/${rand1}/${rand2}/any`
+
+}
+
 
 
 io.on("connection", socket => {
@@ -14,6 +21,7 @@ io.on("connection", socket => {
   users[socket.id] = {userId: currentUserId++}
   socket.on('join',username=>{
     users[socket.id].username = username
+    users[socket.id].avatar = createUserAvatarUrl()
     messageHandler.handleMessage(socket,users)
    
   })
