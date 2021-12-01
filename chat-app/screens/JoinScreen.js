b256e99d1271df0287ddf50e2ab46adff886a1fb
Vaishakh <vaishakhp1998@gmail.com>
2021-12-01 10:02:16 +0530

    stlying to join scree, bigger box and text align

--- chat-app/screens/JoinScreen.js
+++ chat-app/screens/JoinScreen.js
@@ -4,9 +4,14 @@ import {View,Text,TextInput,Image,Button, Platform ,KeyboardAvoidingView} from  
 export default function JoinScreen() {
     return (
         <View style={{flex:1,justifyContent:'center',alignItems:'center',}}>
+
+
         <Image resizeMode='contain' style={{flex:1}} source={require('../assets/chat-icon.png')} />
-        <View style={{flex:1}}>
-        <TextInput placeholder='Enter username' />
+
+        <View style={{flex:1,justifyContent:'space-around',textAlign:'center'}}>
+
+        <TextInput style={{fontSize:30}} placeholder='Enter username' />
+        
         <Button title='Join Chat'/>
         </View>
         {Platform.OS === 'ios' && <KeyboardAvoidingView behavior='padding' />}
